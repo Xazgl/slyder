c0d40d1efe9dbe6d98bca4403e5dd086006fee7e
Uriy Jukov <ldomofon-sericel@rambler.ru>
2021-12-15 16:46:12 +0300

    html and css

---
+++ script.js
@@ -0,0 +1,16 @@
+const next = document.getElementById('Next')
+const prev = document.getElementById('Prewiev')
+
+
+
+const slider = document.querySelector('.slider')
+
+
+
+next.addEventListener('click',function(event) {
+    if(event.target.tagName === 'button') {
+        
+
+
+    } 
+})
